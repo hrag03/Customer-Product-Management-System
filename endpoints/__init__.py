dcc2bd6033d4d81f67fdfc6b8e9c93d046ce45a5
Hrag Sousani <hragsoussani1@gmail.com>
2024-05-15 16:33:43 +0400

    initial commit

---
+++ endpoints/__init__.py
@@ -0,0 +1,3 @@
+from .AuthorizationEndpoint import auth_bp
+from .CustomersEndpoint import customer_bp
+from .ProductsEndpoint import product_bp
---
+++ model/__init__.py
@@ -0,0 +1,1 @@
+from .models import User, UserRole, Product, ProductType, Customer, Warranty, StateGroup, ProductState, CustomerProduct
